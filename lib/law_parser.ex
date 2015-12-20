3e1eecebb0f2d68a68ec5e695d40d67b2d81850e
hiphoox <norberto.ortigoza@raku.mx>
2015-12-20 13:33:59 -0600

    Parsing transitories. We need to check if we will need to parse articles in this section

--- lib/law_parser.ex
+++ lib/law_parser.ex
@@ -2,6 +2,7 @@ defmodule LawExtractor.LawParser do
   @docmodule "LAW = ENCABEZADO TITULO PRELIMINAR LIBRO+ TRANSITORIOS"
 
   @preliminar_article_expression ~r{Artículo \d..-}
+  @transitories_expression ~r(\s\s\sTRANSITORIO\n|\s\s\sTRANSITORIOS\n)
 
   import LawExtractor.Extractor, only: [extract_content_from_file_name: 1]
   import LawExtractor.BookParser, only: [parse_book: 1]
@@ -22,15 +23,24 @@ defmodule LawExtractor.LawParser do
   # Private functions
   ####################
   defp parse_preliminar(preliminar) do
-    preliminar_map = String.split(preliminar, @preliminar_article_expression)
-    |> tl
-    |> Stream.with_index
-    |> Enum.map fn({k, v}) -> {"Artículo #{v + 1}", k} end
+    preliminar_map =
+      preliminar
+      |> String.split(@preliminar_article_expression)
+      |> tl
+      |> Stream.with_index
+      |> Enum.map fn({k, v}) -> {"Artículo #{v + 1}", k} end
+
     Enum.into(preliminar_map, %{})
   end
 
   defp parse_transitories(transitories) do
-    IO.inspect transitories
+    transitories_map =
+      transitories
+      |> String.split(@transitories_expression)
+      |> Stream.with_index
+      |> Enum.map fn({k, v}) -> {"Transitorio #{v + 1}", k} end
+
+    Enum.into(transitories_map, %{})
   end
 
 end
